d69526aa1d7785f32012049d0945abe24a31e350
RevDonz <aprilio842@gmail.com>
2022-03-08 22:13:20 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace tpmodul3_1302204051;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        KodePos kode = new KodePos();
+        Console.WriteLine(kode.getKodePos(KodePos.Kelurahan.Batununggal));
+    }
+}
